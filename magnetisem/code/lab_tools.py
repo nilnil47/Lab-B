d7eac7f3d818d4f8051759403513a5c36bd773c5
Funky-Tortoise <noach.ezerzer@mail.huji.ac.il>
2022-11-20 19:12:22 +0200

    part 3 new

--- magnetisem/code/lab_tools.py
+++ magnetisem/code/lab_tools.py
@@ -3,16 +3,20 @@ import numpy as np
 import hvplot.pandas  # noqa
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import os
+
 
 # pd.set_option('display.precision', 10)
 
 def show_multiple_graphs(d):
     fig, axs = plt.subplots(1, 4, figsize=(9, 3), sharey=True)
     for n, (k, v) in enumerate(d.items()):
-        axs[n-1].plot(v['x'], v['y'])
+        axs[n - 1].plot(v['x'], v['y'])
+
 
 def a():
     pass
+
 
 def convert_number_to_other_base(n, base):
     """
@@ -26,54 +30,62 @@ def convert_number_to_other_base(n, base):
     else:
         return convert_number_to_other_base(n // base, base) + [n % base]
 
+
 def load_data(path):
-    df = pd.read_csv(path, header=None, usecols=[3,4,10], names=['t', 'x', 'y'])
+    df = pd.read_csv(path, header=None, usecols=[3, 4, 10], names=['t', 'x', 'y'])
     return df
+
 
 def harmonic(t, a, w, p):
     return a * np.cos(w * t + p)
 
-def harmonic_fit(df, x='t', y='x', a0=3, w0=1.1, p0=0):
 
-    fit_params, covariances = curve_fit(harmonic, df[x], df[y], p0=[a0, w0, p0], bounds=([0, 0, -2 * np.pi], [np.inf, np.inf, 2 * np.pi]))
+def harmonic_fit(df, x='t', y='x', a0=3, w0=1.1, p0=0):
+    fit_params, covariances = curve_fit(harmonic, df[x], df[y], p0=[a0, w0, p0],
+                                        bounds=([0, 0, -2 * np.pi], [np.inf, np.inf, 2 * np.pi]))
 
     fit = harmonic(df['t'], *fit_params)
 
-    f1 =  plt.figure()
+    f1 = plt.figure()
     plt.plot(df[x], fit)
     plt.plot(df[x], df[y])
 
     return fit_params
 
+
 def find_phase_shift(df, p1, p2, w):
     t = df['t']
 
     t0 = abs(p1 - p2) / w
-    difference_array = np.absolute(t-t0)
+    difference_array = np.absolute(t - t0)
 
     # find the index of minimum element from the array
     n = difference_array.argmin()
     print(f"to index is {n}")
     return n
 
-def fix_phase_shift(df ,n):
+
+def fix_phase_shift(df, n):
     x = list(df['x'][n:])
     y = list(df['y'][:len(df) - n])
     return x, y
-    
 
-def polyarea(x,y):
-    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+
+def polyarea(x, y):
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
 
 def area(p):
-    return 0.5 * abs(sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in segments(p)))
+    return 0.5 * abs(sum(x0 * y1 - x1 * y0 for ((x0, y0), (x1, y1)) in segments(p)))
+
 
 def segments(p):
     return zip(p, p[1:] + [p[0]])
 
-def findzeros(xaxis,yaxis):
-    negativezero = [xaxis[5],yaxis[5]]
-    positivezero = [xaxis[5],yaxis[5]]
+
+def findzeros(xaxis, yaxis):
+    negativezero = [xaxis[5], yaxis[5]]
+    positivezero = [xaxis[5], yaxis[5]]
     for k in range(len(yaxis)):
         if abs(yaxis[k]) <= abs(negativezero[1]) and xaxis[k] <= 0:
             negativezero[1] = yaxis[k]
@@ -81,8 +93,37 @@ def findzeros(xaxis,yaxis):
         elif abs(yaxis[k]) <= abs(negativezero[1]) and xaxis[k] >= 0:
             positivezero[1] = yaxis[k]
             positivezero[0] = xaxis[k]
-    return (positivezero,negativezero)
+    return (positivezero, negativezero)
 
-def find_hardness(xaxis,yaxis):
-    positivezero,negativezero = findzeros(xaxis,yaxis)
+
+def find_hardness(xaxis, yaxis):
+    positivezero, negativezero = findzeros(xaxis, yaxis)
     return abs(positivezero[0] - negativezero[0]) / 2
+
+
+def find_peak(y_axis, x_axis):
+    peak = [x_axis[1], y_axis[1]]
+    for k in range(len(y_axis)):
+        if y_axis[k] >= peak[1]:
+            peak[1] = y_axis[k]
+            peak[0] = x_axis[k]
+    return peak
+
+
+def read_to_dict(file):
+    csv_files = os.listdir(file)
+    data_dict = {}
+    for x in csv_files:
+        path = os.path.join(file, x)
+        data_dict[os.path.splitext(os.path.basename(path))[0]] = load_data(x)
+    return data_dict
+
+
+def p_graph(data_dict):
+    permeability = []
+    H_filed = []
+    for key in data_dict.keys():
+        L = find_peak(data_dict[key]['y'], data_dict[key]['x'])
+        permeability.append(L[1] / (abs(L[0])))
+        H_filed.append(abs(L[0]))
+    return [H_filed, permeability]
