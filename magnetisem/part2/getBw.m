bd141c93533a5d0c2652ae5e59460c4e088a5050
nilnil47 <nilnil47@gmail.com>
2022-11-20 20:43:15 +0200

    working

--- magnetisem/part2/getBw.m
+++ magnetisem/part2/getBw.m
@@ -1,4 +1,4 @@
-function [voltages, bwValues,bwDeltaNeg, bwDeltaPos, g] = getBw(folder)
+function [voltages, bwValues] = getBw(folder)
 %GETBW Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,31 +11,32 @@ voltages = [];
 bmpFiles = dir(folder + "*.bmp");
 
 % callibrate
-for i = 1 : size(bmpFiles,1)
-    image = imread(strcat(bmpFiles(i).folder ,'/' ,bmpFiles(i).name));
-    grayThreshes(end+1) = graythresh(image);
-end
+% for i = 1 : size(bmpFiles,1)
+%     image = imread(strcat(bmpFiles(i).folder ,'/' ,bmpFiles(i).name));
+%     grayThreshes(end+1) = adaptthresh(image, 'ForegroundPolarity', 'dark');
+% end
 
 % calculating the systersis
-g = grayThreshes;
-grayThreshes(find(grayThreshes > max(grayThreshes)-0.02)) = [];
-grayThreshes(find(grayThreshes < min(grayThreshes)+0.02)) = [];
-threshold = mean(grayThreshes);
-thresholdSTD = std(grayThreshes);
+% g = grayThreshes;
+% grayThreshes(find(grayThreshes > max(grayThreshes)-0.02)) = [];
+% grayThreshes(find(grayThreshes < min(grayThreshes)+0.02)) = [];
+% threshold = mean(grayThreshes);
+% thresholdSTD = std(grayThreshes);
 
 for i = 1 : size(bmpFiles,1)
     image = imread(strcat(bmpFiles(i).folder ,'/' ,bmpFiles(i).name));
     [filepath,name,ext] = fileparts(bmpFiles(i).name);
     voltages(end+1) = str2num(name);
-    imageBW = imbinarize(image,threshold,'ForegroundPolarity','dark');
+    T = adaptthresh(image, 'ForegroundPolarity', 'dark');
+    imageBW = imbinarize(image,T);
     bwValues(end+1) = mean(imageBW,'all');
-    imageBWLow = imbinarize(image,threshold + thresholdSTD, 'ForegroundPolarity', 'dark');
-    bwValuesLow(end+1) = mean(imageBWLow,'all');
-    imageBWHigh = imbinarize(image,threshold - thresholdSTD, 'ForegroundPolarity', 'dark');
-    bwValuesHigh(end+1) = mean(imageBWHigh,'all');
+%     imageBWLow = imbinarize(image,threshold + thresholdSTD, 'ForegroundPolarity', 'dark');
+%     bwValuesLow(end+1) = mean(imageBWLow,'all');
+%     imageBWHigh = imbinarize(image,threshold - thresholdSTD, 'ForegroundPolarity', 'dark');
+%     bwValuesHigh(end+1) = mean(imageBWHigh,'all');
 end
 
-bwDeltaPos = bwValuesHigh - bwValues;
-bwDeltaNeg = bwValues - bwValuesLow;
+% bwDeltaPos = bwValuesHigh - bwValues;
+% bwDeltaNeg = bwValues - bwValuesLow;
 end
 
--- magnetisem/part2/magnetic.m
+++ magnetisem/part2/magnetic.m
@@ -1,12 +1,12 @@
 forwardFolder = "/Users/user/Documents/semster_c/courses/lab/magnetisem/part2/week3/ex2/forward/";
 backwardFolder = "/Users/user/Documents/semster_c/courses/lab/magnetisem/part2/week3/ex2/backward/";
 
-[voltagesFirst, bwFirst, bwDeltaNegFirst, bwDeltaPosFirst, g1] = getBw(forwardFolder);
-[voltagesSecond, bwSecond , bwDeltaNegSecond, bwDeltaPosSecond, g2] = getBw(backwardFolder);
+[voltagesFirst, bwFirst] = getBw(forwardFolder);
+[voltagesSecond, bwSecond] = getBw(backwardFolder);
 
-e1 = errorbar(voltagesFirst, bwFirst, bwDeltaNegFirst, bwDeltaPosFirst, '*');
+e1 = plot(voltagesFirst, bwFirst, '*');
 hold on
-e2 = errorbar(-voltagesSecond, bwSecond, bwDeltaNegSecond, bwDeltaPosSecond, '*');
+e2 = plot(voltagesSecond, bwSecond, '*');
 
 e1.LineWidth = 0.9;
 e2.LineWidth = 0.9;
@@ -18,9 +18,9 @@ ylabel('Normalized Average Brightness','FontSize', 55);
 set(gca,'FontSize',25);
 grid on;
 
-figure
-p3 = plot([g1, g2],'*');
-xlabel('# Of Picture','FontSize', 25);
-ylabel('Gray Threshold','FontSize', 25);
-set(gca,'FontSize',25);
+% figure
+% p3 = plot([g1, g2],'*');
+% xlabel('# Of Picture','FontSize', 25);
+% ylabel('Gray Threshold','FontSize', 25);
+% set(gca,'FontSize',25);
 
